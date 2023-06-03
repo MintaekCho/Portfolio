acb1137339b0b098e2a5556f8f3040a0fe15461f
Mintaek_CHO <mintaek5555@gmail.com>
2023-06-03 09:29:19 +0900

    feat: Footer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
   const [categoryVisible, setCategoryVisible] = useState(false);
   const handleCategory = () => setCategoryVisible(!categoryVisible)
   return (
-    <header className="sticky top-0 left-0 w-full min-w-[430px] px-10 max-w-7xl z-9">
+    <header className="sticky mt-10 top-0 left-0 w-full min-w-[430px] px-10 max-w-7xl z-9 hover:bg-white/10 rounded-2xl">
       <nav className="flex justify-center items-center p-4 relative">
         <div className="text-white text-4xl absolute left-0">
           <GiHamburgerMenu className="cursor-pointer hover:scale-110 ease-in-out duration-300" onClick={handleCategory}/>
