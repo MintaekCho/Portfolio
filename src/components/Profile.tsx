8c6dfb73dee78425ac76f9de6ccb74342fe0d3d7
Mintaek_CHO <mintaek5555@gmail.com>
2023-06-04 12:36:50 +0900

    feat: 화살표 레이아웃 구성 변경, 프로필 이미지명 변경

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,30 +4,41 @@ import React, { useState } from "react";
 import { BsFillCaretLeftFill, BsFillCaretRightFill } from "react-icons/bs";
 
 export default function Profile() {
-  const [count, setCount] = useState<number>(0);
+  const [count, setCount] = useState<number>(1);
   const handleUpCount = () => {
-    if (count > 2) setCount(0);
+    if (count > 3) setCount(1);
     else setCount((count) => count + 1);
   };
 
   const handleDownCount = () => {
-    if (count < 1) setCount(3);
+    if (count < 2) setCount(4);
     else setCount((count) => count - 1);
   };
   return (
-    <article className="flex items-center gap-4 my-10">
-      <div className="text-5xl">
-        <BsFillCaretLeftFill className="cursor-pointer" onClick={handleDownCount} />
-      </div>
+    <article className="flex flex-col items-center justify-center gap-4 my-10">
       <Image
-      className="rounded-3xl hover:scale-105 ease-in-out duration-300"
+        className="rounded-3xl hover:scale-105 ease-in-out duration-300"
         src={`/images/profile${count}.jpeg`}
         alt={`profile${count}`}
         width={500}
         height={600}
       />
-      <div className="text-5xl">
-        <BsFillCaretRightFill className="cursor-pointer" onClick={handleUpCount} />
+      <div className="flex gap-10 items-center mt-6">
+        <div className="text-5xl">
+          <BsFillCaretLeftFill
+            className="cursor-pointer hover:text-[#c4c4c4]"
+            onClick={handleDownCount}
+          />
+        </div>
+        <p>
+          {count} / <span className="font-bold">4</span>
+        </p>
+        <div className="text-5xl">
+          <BsFillCaretRightFill
+            className="cursor-pointer hover:text-[#c4c4c4]"
+            onClick={handleUpCount}
+          />
+        </div>
       </div>
     </article>
   );
--- src/components/projects/ImageWrap.tsx
+++ src/components/projects/ImageWrap.tsx
@@ -2,10 +2,16 @@ import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import { BsFillCaretLeftFill, BsFillCaretRightFill } from "react-icons/bs";
 
-export default function ImageWrap({title, imageCount}: {title:string, imageCount:number}) {
+export default function ImageWrap({
+  title,
+  imageCount,
+}: {
+  title: string;
+  imageCount: number;
+}) {
   const [count, setCount] = useState<number>(1);
   const handleUpCount = () => {
-    if (count > imageCount-1) setCount(1);
+    if (count > imageCount - 1) setCount(1);
     else setCount((count) => count + 1);
   };
 
@@ -15,34 +21,37 @@ export default function ImageWrap({title, imageCount}: {title:string, imageCount
   };
 
   useEffect(() => {
-    setCount(1)
-  }, [imageCount])
+    setCount(1);
+  }, [imageCount]);
 
   return (
     <>
-      <div className="flex gap-4 items-center justify-center">
-        <div className="text-5xl">
-          <BsFillCaretLeftFill
-            className="cursor-pointer"
-            onClick={handleDownCount}
-          />
-        </div>
-
+      <div className="flex flex-col gap-4 items-center justify-center">
         <Image
           src={`/images/projects/${title}/${title}${count}.png`}
           alt={"projectImage"}
           width={800}
           height={600}
-          className='ease-in-out duration-500'
+          className="ease-in-out duration-500"
         />
-        <div className="text-5xl">
-          <BsFillCaretRightFill
-            className="cursor-pointer"
-            onClick={handleUpCount}
-          />
+        <div className="flex items-center gap-10 mb-4">
+          <div className="text-5xl">
+            <BsFillCaretLeftFill
+              className="cursor-pointer hover:text-[#c4c4c4]"
+              onClick={handleDownCount}
+            />
+          </div>
+          <p className="text-lg">
+            {count} / <span className="font-bold">{imageCount}</span>
+          </p>
+          <div className="text-5xl">
+            <BsFillCaretRightFill
+              className="cursor-pointer hover:text-[#c4c4c4]"
+              onClick={handleUpCount}
+            />
+          </div>
         </div>
       </div>
-      <p className="text-lg">{count} / {imageCount}</p>
     </>
   );
 }
