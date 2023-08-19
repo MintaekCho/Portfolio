3e218df295d9677af4babd50cd1680cfebe80991
Mintaek_CHO <mintaek5555@gmail.com>
2023-08-19 21:21:32 +0900

    metaData 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import Header from "@/components/Header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "조민택 포트폴리오",
+  title: "조민택 포트폴리오 | mintaek5555@gmail.com",
   description: "ABOUT | SKILL | PROJECT",
   icons: {
     icon: '/favicion.ico'
